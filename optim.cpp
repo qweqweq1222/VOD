a29cbafec4e0f4f89d132d0606a638e0daf757eb
qweqweq1222 <quankyyam@gmail.com>
2022-12-04 21:52:47 +0300

    Update optim.cpp

--- optim.cpp
+++ optim.cpp
@@ -22,156 +22,33 @@ void Display(const Mat& mtx)
 	}
 }
 
-vector<double> GetAnglesAndVec(const Mat& Rt)
+vector<double> GetAnglesAndVec(const Mat Rt)
 {
-	double beta = asin(Rt.at<float>(0, 2));
-	double alpha = acos(Rt.at<float>(2, 2) / cos(beta));
-	double gamma = acos(Rt.at<float>(0, 0) / cos(beta));
+	double alpha, beta, gamma;
+	if(abs(Rt.at<float>(0, 2)) < 1)
+		beta = asin(Rt.at<float>(0, 2));
+	else if(Rt.at<float>(0,2) == 1)
+		beta = M_PI / 2;
+	else if(Rt.at<float>(0, 2) == -1)
+		beta = -M_PI / 2;
+
+	if(abs(Rt.at<float>(2, 2) / cos(beta)) < 1)
+		alpha = acos(Rt.at<float>(2, 2) / cos(beta));
+	else if (Rt.at<float>(2, 2) / cos(beta) == 1)
+		beta = 0;
+	else if (Rt.at<float>(2, 2) / cos(beta) == -1)
+		beta = M_PI;
+
+	if(abs(Rt.at<float>(0, 0) / cos(beta)) < 1)
+		gamma = acos(Rt.at<float>(0, 0) / cos(beta));
+	else if (Rt.at<float>(0, 0) / cos(beta) == 1)
+		beta = 0;
+	else if (Rt.at<float>(0, 0) / cos(beta) == -1)
+		beta = M_PI;
 	return { alpha, beta, gamma, Rt.at<float>(0,3), Rt.at<float>(1,3), Rt.at<float>(2,3) };
 }
-/*void EstimateAndOptimize(const std::string& left_path, const std::string& right_path, const std::string& input, const Mat& PLeft, const Mat& PRight)
-{
-	CameraInfo cil = Decompose(PLeft);
-	CameraInfo cir = Decompose(PRight);
-	fs::directory_iterator left_iterator(left_path);
-	fs::directory_iterator right_iterator(right_path);
-	fs::directory_iterator next_iterator(left_path);
 
-	std::advance(left_iterator, START_KEY_FRAME);
-	std::advance(right_iterator, START_KEY_FRAME);
-	std::advance(next_iterator, START_KEY_FRAME + 1);
-	std::vector<std::vector<KeyPoint>> vec;
-	std::ofstream in(input);
-	int buffer = 0;
-	int counter = 0;
-	Mat GLOBAL_P = Mat::eye(4, 4, CV_32F);
 
-	for (int i = 0; i < NUM_OF_FRAMES; i += buffer)
-	{
-		std::vector<std::vector<double>> T_i;
-		fs::directory_iterator copy_left(left_path);
-		fs::directory_iterator copy_next(left_path);
-
-		std::advance(copy_next, START_KEY_FRAME + counter + 1);
-		std::advance(copy_left, START_KEY_FRAME + counter);
-
-		vec = GetSamePoints(copy_left, copy_next, SAME_POINTS);
-		cout << " SZIE :: " << vec.size() << endl;
-		counter += vec.size();
-		buffer = vec.size();
-		std::vector<Point3f> pts3d;
-
-		Mat P = Mat::eye(4, 4, CV_32F);
-		for (int jdx = 0; jdx < vec.size() - 1; ++jdx)
-		{
-
-			std::pair<Mat, Mat> buffer = EstimateMotion(imread((*left_iterator).path().u8string()),
-				imread((*right_iterator).path().u8string()),
-				imread((*next_iterator).path().u8string()), PLeft, PRight);
-			if (jdx == 0)
-			{
-				Mat disparity = CalculateDisparity(imread((*left_iterator).path().u8string()), imread((*next_iterator).path().u8string()));
-				float coeff = cil.cameraMatrix.at<float>(0, 0) * (cir.transVector.at<float>(0) - cil.transVector.at<float>(0));
-
-				for (auto& pt : vec[0]) {
-					float u = (float(pt.pt.x));
-					float v = (float(pt.pt.y));
-					float z = coeff / disparity.at<float>(int(v), int(u));
-					if (z > 0 && z < DEPTH_TRASH)
-					{
-						float x = z * (u - cil.cameraMatrix.at<float>(0, 2)) / cil.cameraMatrix.at<float>(0, 0);
-						float y = z * (v - cil.cameraMatrix.at<float>(1, 2)) / cil.cameraMatrix.at<float>(1, 1);
-						pts3d.push_back({ x, y, z });
-					}
-				}
-			}
-
-			++left_iterator;
-			++right_iterator;
-			++next_iterator;
-
-			Mat rot;
-			cv::Rodrigues(buffer.first, rot);
-			rot.convertTo(rot, CV_32F);
-			buffer.second.convertTo(buffer.second, CV_32F);
-			Mat L = T(rot, buffer.second);
-			P *= L.inv();
-			std::vector<double> vector = Transform_vec(P);
-			T_i.push_back(vector);
-		}
-		ceres::Problem problem;
-		std::vector<double*> crt(T_i.size());
-		for (int i = 0; i < T_i.size(); ++i)
-		{
-			crt[i] = new double[12];
-			for (int j = 0; j < 12; ++j)
-				crt[i][j] = double(T_i[i][j]);
-		}
-		std::vector<double*> pts_3d(pts3d.size());
-		for (int i = 0; i < pts3d.size(); ++i)
-		{
-			pts_3d[i] = new double[3];
-			pts_3d[i][0] = double(pts3d[i].x);
-			pts_3d[i][1] = double(pts3d[i].y);
-			pts_3d[i][2] = double(pts3d[i].z);
-		}
-		double buffer_mas[12];
-		for (int i = 0; i < vec.size() - 1; ++i)
-		{
-			for (int j = 0; j < pts3d.size(); ++j)
-			{
-				ceres::CostFunction* cost_function =
-					SnavelyReprojectionError::Create(double(vec[i + 1][j].pt.x), double(vec[i + 1][j].pt.y),
-						PLeft.at<float>(0,0), PLeft.at<float>(0, 0), PLeft.at<float>(0, 2), PLeft.at<float>(1, 2));
-				problem.AddResidualBlock(cost_function, nullptr, crt[i], pts_3d[j]);
-				for (int idx = 0; idx < 9; ++idx)
-				{
-					double buffer = crt[i][idx];
-					problem.SetParameterLowerBound(crt[i], idx, -1.0f);
-					//problem.SetParameterLowerBound(crt[i], idx, buffer - fabs(buffer) * 0.3);
-					problem.SetParameterUpperBound(crt[i], idx, 1.0f);
-					//problem.SetParameterUpperBound(crt[i], idx, buffer + fabs(buffer) * 0.3);
-				}
-				for (int idx = 9; idx < 12; ++idx)
-				{
-					double buffer = crt[i][idx];
-					if (idx != 11)
-					{
-						problem.SetParameterLowerBound(crt[i], idx, buffer - fabs(buffer) * 0.3);
-						problem.SetParameterUpperBound(crt[i], idx, buffer + fabs(buffer) * 0.3);
-					}
-					else
-					{
-						problem.SetParameterLowerBound(crt[i], idx, 0.8 * buffer);
-						problem.SetParameterUpperBound(crt[i], idx, buffer * 1.2);
-					}
-				}		
-			}
-		}
-		ceres::Solver::Options options;
-		options.linear_solver_type = ceres::DENSE_SCHUR;
-		options.minimizer_progress_to_stdout = true;
-		ceres::Solver::Summary summary;
-		ceres::Solve(options, &problem, &summary);
-		for (int i = 0; i < crt.size(); ++i)
-		{
-			for (int j = 0; j < 12; ++j)
-				std::cout << crt[i][j] << " ";
-			std::cout << std::endl;
-			Mat copy_GLOBAL = GLOBAL_P.clone();
-			copy_GLOBAL *= FromPointerToMat(crt[i]);
-			in << copy_GLOBAL.at<float>(0, 3) << " " << copy_GLOBAL.at<float>(1, 3) << " " << copy_GLOBAL.at<float>(2, 3) << "\n";
-			if (i == crt.size() - 1)
-				GLOBAL_P *= FromPointerToMat(crt[i]);
-		}
-
-		for (auto& p : crt)
-			delete[] p;
-		for (auto& d : pts_3d)
-			delete[] d;
-
-	}
-}*/
 Mat ReconstructFromV4(double* alpha_trans)
 {
 	Mat answer = Mat::eye(4, 4, CV_32F);
@@ -182,6 +59,26 @@ Mat ReconstructFromV4(double* alpha_trans)
 	answer.at<float>(0, 3) = alpha_trans[1];
 	answer.at<float>(1, 3) = alpha_trans[2];
 	answer.at<float>(2, 3) = alpha_trans[3];
+	return answer;
+}
+Mat ReconstructFromV6(double* alpha_trans)
+{
+	Mat answer = Mat::eye(4, 4, CV_32F);
+	double a = alpha_trans[0];
+	double b = alpha_trans[1];
+	double g = alpha_trans[2];
+	answer.at<float>(0, 0) = cos(b) * cos(g);
+	answer.at<float>(0, 1) = -sin(g) * cos(b);
+	answer.at<float>(0, 2) = sin(b);
+	answer.at<float>(1, 0) = sin(a) * sin(b) * cos(g) + sin(g) * cos(a);
+	answer.at<float>(1, 1) = -sin(a) * sin(b) * sin(g) + cos(g) * cos(a);
+	answer.at<float>(1, 2) = -sin(a) * cos(b);
+	answer.at<float>(2, 0) = sin(a) * sin(g) - sin(b) * cos(a) * cos(g);
+	answer.at<float>(2, 1) = sin(a) * cos(g) + sin(b) * sin(g) * cos(a);
+	answer.at<float>(2, 2) = cos(a) * cos(b);
+	answer.at<float>(0, 3) = alpha_trans[3];
+	answer.at<float>(1, 3) = alpha_trans[4];
+	answer.at<float>(2, 3) = alpha_trans[5];
 	return answer;
 }
 void EstimateAndOptimize(const std::string& left_path, const std::string& right_path, const std::string& input, const Mat& PLeft, const Mat& PRight)
@@ -286,16 +183,22 @@ void EstimateAndOptimize(const std::string& left_path, const std::string& right_
 			buffer.second.convertTo(buffer.second, CV_32F);
 			Mat L = T(rot, buffer.second);
 			P *= L.inv();
-			std::vector<double> a_t = {acos(P.at<float>(0,0)),P.at<float>(0, 3), P.at<float>(1, 3), P.at<float>(2, 3)};
+			//std::vector<double> a_t = {acos(P.at<float>(0,0)),P.at<float>(0, 3), P.at<float>(1, 3), P.at<float>(2, 3)};
+			std::vector<double> a_t = GetAnglesAndVec(P);
 			location.push_back(a_t);
 		}
 		ceres::Problem problem;
-		vector<double*> alphas_trans(location.size());
+		const int b = location.size();
+		vector<double*> alphas_trans_(location.size());
 		for (int i = 0; i < location.size(); ++i)
 		{
-			alphas_trans[i] = new double[4];
-			for (int j = 0; j < 4; ++j)
-				alphas_trans[i][j] = location[i][j];
+			alphas_trans_[i] = new double[6];
+			alphas_trans_[i][0] = location[i][0];
+			alphas_trans_[i][1] = location[i][1];
+			alphas_trans_[i][2] = location[i][2];
+			alphas_trans_[i][3] = location[i][3];
+			alphas_trans_[i][4] = location[i][4];
+			alphas_trans_[i][5] = location[i][5];
 		}
 		std::vector<double*> pts_3d(pts3d.size());
 		for (int i = 0; i < pts3d.size(); ++i)
@@ -305,7 +208,6 @@ void EstimateAndOptimize(const std::string& left_path, const std::string& right_
 			pts_3d[i][1] = pts3d[i][1];
 			pts_3d[i][2] = pts3d[i][2];
 		}
-		double buffer_mas[12];
 		
 		for (int i = 0; i < alternative.size() - 1; ++i)
 		{
@@ -315,41 +217,53 @@ void EstimateAndOptimize(const std::string& left_path, const std::string& right_
 					/* imbalance checker */
 
 					double P3[3];
-					P3[0] = cos(alphas_trans[i][0]) * (pts_3d[j][0] - alphas_trans[i][1]) - sin(alphas_trans[i][0]) * (pts_3d[j][2] - alphas_trans[i][3]);
-					P3[1] = pts_3d[j][1] - alphas_trans[i][2];
-					P3[2] = sin(alphas_trans[i][0]) * (pts_3d[j][0] - alphas_trans[i][1]) + cos(alphas_trans[i][0]) * (pts_3d[j][2] - alphas_trans[i][3]);
-					double a = PLeft.at<float>(0, 0) * pts_3d[j][0] / pts_3d[j][2] + PLeft.at<float>(0, 2);
-					double b = PLeft.at<float>(0, 0) * pts_3d[j][1] / pts_3d[j][2] + PLeft.at<float>(1, 2);
-					a -= double(alternative[i + 1][j].pt.y);
-					b -= double(alternative[i + 1][j].pt.x);
-
-					if (abs(a) < 100 && abs(b) < 100)
+					double a = alphas_trans_[i][0];
+					double b = alphas_trans_[i][1];
+					double g = alphas_trans_[i][2];
+					double dx = pts_3d[j][0] - alphas_trans_[i][3];
+					double dy = pts_3d[j][1] - alphas_trans_[i][4];
+					double dz = pts_3d[j][2] - alphas_trans_[i][5];
+					P3[0] = (cos(b) * cos(g)) * dx + (sin(a) * sin(b) * cos(g) + sin(g) * cos(a)) * dy + (sin(a) * sin(g) - sin(b) * cos(a) * cos(g)) * dz;
+					P3[1] = (-sin(g) * cos(b)) * dx + (cos(a) * cos(g) - sin(a) * sin(b) * sin(g)) * dy + (sin(a) * cos(g) + sin(b) * sin(g) * cos(a)) * dz;
+					P3[2] = (sin(b)) * dx + (-sin(a) * cos(b)) * dy + (cos(a) * cos(b)) * dz;
+					double predicted_x = PLeft.at<float>(0, 0) * P3[0] / P3[2] + PLeft.at<float>(0, 2);
+					double predicted_y = PLeft.at<float>(0, 0) * P3[1] / P3[2] + PLeft.at<float>(1, 2);
+					/*
+					double dx = pts_3d[j][0] - alphas_trans_[i][1];
+					double dy = pts_3d[j][1] - alphas_trans_[i][2];
+					double dz = pts_3d[j][2] - alphas_trans_[i][3];
+					double a = alphas_trans_[i][0];
+					P3[0] = cos(a) * dx - sin(a) * dz;
+					P3[1] = dy;
+					P3[2] = sin(a) * dx + cos(a) * dz;
+					double predicted_x = PLeft.at<float>(0, 0) * P3[0] / P3[2] + PLeft.at<float>(0, 2);
+					double predicted_y = PLeft.at<float>(0, 0) * P3[1] / P3[2] + PLeft.at<float>(1, 2);
+					*/
+					if (abs(predicted_x - alternative[i + 1][j].pt.y) < 100 && abs(predicted_y - alternative[i + 1][j].pt.x) < 100)
 					{
-						ceres::CostFunction* cost_function =
-							SnavelyReprojectionError::Create(double(alternative[i + 1][j].pt.y), double(alternative[i + 1][j].pt.x),
+						ceres::CostFunction* cost_function = SnavelyReprojectionError::Create(double(alternative[i + 1][j].pt.y), double(alternative[i + 1][j].pt.x),
 								PLeft.at<float>(0, 0), PLeft.at<float>(0, 0), PLeft.at<float>(0, 2), PLeft.at<float>(1, 2));
-						problem.AddResidualBlock(cost_function, nullptr, alphas_trans[i], pts_3d[j]);
-						problem.SetParameterLowerBound(alphas_trans[i], 0, -M_PI / 2);
-						problem.SetParameterUpperBound(alphas_trans[i], 0, M_PI / 2);
+						problem.AddResidualBlock(cost_function, nullptr, alphas_trans_[i], pts_3d[j]);
+						/*for (int ai = 0; ai < 3; ++ai)
+						{
+							problem.SetParameterLowerBound(alphas_trans[i], ai, -M_PI / 2);
+							problem.SetParameterUpperBound(alphas_trans[i], ai, M_PI / 2);
+						}*/
+					
+						problem.SetParameterLowerBound(alphas_trans_[i], 0, -M_PI / 2);
+						problem.SetParameterUpperBound(alphas_trans_[i], 0, M_PI / 2);
 						for (int index = 1; index < 4; ++index)
 						{
 							if (index != 2) //30% вариации 
 							{
-								double abs_ = abs(alphas_trans[i][index]);
-								double lower_bound = alphas_trans[i][index] - 0.3 * abs_;
-								double upper_bound = alphas_trans[i][index] + 0.3 * abs_;
-								problem.SetParameterLowerBound(alphas_trans[i], index, lower_bound);
-								problem.SetParameterUpperBound(alphas_trans[i], index, upper_bound);
+								double abs_ = abs(alphas_trans_[i][index]);
+								double lower_bound = alphas_trans_[i][index] - 0.3 * abs_;
+								double upper_bound = alphas_trans_[i][index] + 0.3 * abs_;
 							}
-							else //по y (вертикали) мы почти никуда не смещаемся - 5%
+							else
 							{
-								double abs_ = abs(alphas_trans[i][index]);
-								double lower_bound = alphas_trans[i][index] - 0.01 * abs_;
-								double upper_bound = alphas_trans[i][index] + 0.01 * abs_;
-								problem.SetParameterLowerBound(alphas_trans[i], index, lower_bound);
-								problem.SetParameterUpperBound(alphas_trans[i], index, upper_bound);
-								problem.SetParameterLowerBound(alphas_trans[i], index, -1.0);
-								problem.SetParameterUpperBound(alphas_trans[i], index, 1.0);
+								problem.SetParameterLowerBound(alphas_trans_[i], 4, -1.0);
+								problem.SetParameterUpperBound(alphas_trans_[i], 4, 1.0);
 							}
 						}
 					}
@@ -375,21 +289,37 @@ void EstimateAndOptimize(const std::string& left_path, const std::string& right_
 					//cout << "{" << alphas_trans[i][0] << "," << alphas_trans[i][1] << "," << alphas_trans[i][2] << "," << alphas_trans[i][3] << "\n";
 			}
 		}
+		/*for (auto& el : deep_copy_location)
+		{
+			for (auto& ge : el)
+				cout << ge << " ";
+			cout << endl;
+		}*/
 		ceres::Solver::Options options;
 		options.linear_solver_type = ceres::DENSE_SCHUR;
 		options.minimizer_progress_to_stdout = true;
 		ceres::Solver::Summary summary;
 		ceres::Solve(options, &problem, &summary);
-		for (int i = 0; i < alphas_trans.size(); ++i)
+		/*for (auto& el : deep_copy_location)
+		{
+			for (auto& ge : el)
+				cout << ge << " ";
+			cout << endl;
+		}*/
+		for (int unique_i = 0; unique_i < location.size(); ++unique_i)
 		{
 			Mat copy_GLOBAL = GLOBAL_P.clone();
-			copy_GLOBAL *= ReconstructFromV4(alphas_trans[i]);
+			for (int unique_j = 0; unique_j < 4; ++unique_j)
+				if (isnan(alphas_trans_[unique_i][unique_j]))
+					alphas_trans_[unique_i][unique_j] = location[unique_i][unique_j];
+					//cout << deep_copy_location[unique_i][unique_j] << ", " << alphas_trans[unique_i][unique_j] << endl;
+			copy_GLOBAL *= ReconstructFromV6(alphas_trans_[unique_i]);
 			in << copy_GLOBAL.at<float>(0, 3) << " " << copy_GLOBAL.at<float>(1, 3) << " " << copy_GLOBAL.at<float>(2, 3) << "\n";
-			if (i == alphas_trans.size() - 1)
-				GLOBAL_P *= ReconstructFromV4(alphas_trans[i]);
+			if (unique_i == location.size() - 1)
+				GLOBAL_P *= ReconstructFromV6(alphas_trans_[unique_i]);
 		}
 
-		for (auto& p : alphas_trans)
+		for (auto &p : alphas_trans_)
 			delete[] p;
 		for (auto& d : pts_3d)
 			delete[] d;
